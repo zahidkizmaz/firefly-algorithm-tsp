ff07825b5354319f126f0d051a74d997c976f7d5
zahidkizmaz <mzkizmaz@gmail.com>
2019-05-27 15:37:29 +0300

    logging added. Better graph names. Bug fixes in packages

--- src/main.jl
+++ src/main.jl
@@ -12,19 +12,13 @@ include("./inversion_mutation.jl")
 include("./path_cost.jl")
 include("./read_nodes.jl")
 
-using Pkg, ProgressBars, PyPlot, Random
-
-import NodeModule.Node, FireflyModule.Firefly
+using Pkg, Random
 
 Pkg.update()
 Pkg.add("PyPlot")
-Pkg.add("ProgressBars")
 
-LIGHT_ABSORPTION_COEFF = 0.2
-ATTRACTION_COEFF = 1
-ITERATION_NUMBER = 5
-POPULATION_NUMBER = 12
-NUMBER_OF_MUTATION = 8
+import NodeModule.Node, FireflyModule.Firefly
+using PyPlot, Logging
 
 file_name = get(ENV, "TSP_FILE", nothing)
 if file_name != nothing
@@ -33,12 +27,34 @@ if file_name != nothing
     distance_matrix = create_distance_matrix(nodes)
 end
 
+# Checking if logs direcitory is available!
+# If is not create one!
+if !isdir("logs") mkdir("logs"); end
+
+if '/' in file_name
+    tsp_file_name = split(file_name, "/")[end]
+    io_log = open("logs/$tsp_file_name.txt", "w+")
+else
+    tsp_file_name = file_name
+    io_log = open("logs/$tsp_file_name.txt", "w+")
+end
+
+# Setting Logger!
+logger = SimpleLogger(io_log)
+global_logger(logger)
+
+
+LIGHT_ABSORPTION_COEFF = 0.2
+ATTRACTION_COEFF = 1
+ITERATION_NUMBER = 10 
+POPULATION_NUMBER = 20 
+NUMBER_OF_MUTATION = 10
+
 bests = []
 pop = [Firefly(copy(nodes), -1.0) for _ in 1:POPULATION_NUMBER]
 init_firefly_paths(pop, distance_matrix)
 println(length(pop), " Sized population created!")
-for t in tqdm(1:ITERATION_NUMBER)
-    print("Step:",t, "-")
+for t in 1:ITERATION_NUMBER
     new_pop = []
     for f1 in pop
         for f2 in pop
@@ -55,16 +71,24 @@ for t in tqdm(1:ITERATION_NUMBER)
         pop!(pop)
     end
     push!(bests, sorted_pop[1])
+    current_best_cost = copy(bests[end].cost)
+    println("Step:",t, " - Best ", bests[end])
+    @info("Step:$t, Population Size:$POPULATION_NUMBER, Mutation Number:$NUMBER_OF_MUTATION",
+          current_best_cost)
 end
 
+close(io_log)
+
+# Checking if graphs direcitory is available for saving graphs!
+# If is not create one!
+if !isdir("graphs") mkdir("graphs"); end
+
 # Plotting and printing the best solutions.
-println("Bests: ",bests)
 best = sort(pop, by=p->p.cost)[1]
 best_x = [n.x for n in best.path]
 best_y = [n.y for n in best.path]
 best_cost = best.cost
 plot(best_x, best_y, "-")
-
 x = [n.x for n in nodes]
 y = [n.y for n in nodes]
 plot(x, y, "ro", markersize=2.0)
@@ -72,5 +96,16 @@ xlabel("X Dimension")
 ylabel("Y Dimension")
 title("Firefly Cost: $best_cost", fontsize=10)
 suptitle("File: $file_name", fontweight="bold")
-if !isdir("graphs") mkdir("graphs"); end
-savefig("graphs/cities.png")
+fig_name = "graphs/best_firefly_path_$tsp_file_name-cost_$best_cost.png"
+savefig(fig_name)
+println("Saved figure:",fig_name)
+clf()
+
+x_cost = [b.cost for b in bests]
+plot(x_cost )
+xlabel("Path Cost")
+ylabel("Iteration Number")
+title("Firefly Iteration Graphic", fontsize=12)
+fig_name = "graphs/iteration_$tsp_file_name-cost_$best_cost.png"
+savefig(fig_name)
+println("Saved figure:",fig_name)
